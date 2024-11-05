c2b8c4a07eedab5fcd1d61ff63f99a96366f65cb
Constanza <cons.lopezv@duocuc.cl>
2024-11-05 16:29:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontend_FixFinder',
+  webDir: 'www'
+};
+
+export default config;
