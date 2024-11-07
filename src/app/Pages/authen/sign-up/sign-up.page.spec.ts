942949f46de68077a81b3d3f9167edfbcfff6043
Constanza <cons.lopezv@duocuc.cl>
2024-11-07 01:15:26 -0300

    edicion

--- src/app/Pages/authen/sign-up/sign-up.page.spec.ts
+++ src/app/Pages/authen/sign-up/sign-up.page.spec.ts
@@ -1,12 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RegistrarPage } from './registrar.page';
+import { SignUpPage } from './sign-up.page';
 
-describe('RegistrarPage', () => {
-  let component: RegistrarPage;
-  let fixture: ComponentFixture<RegistrarPage>;
+describe('SignUpPage', () => {
+  let component: SignUpPage;
+  let fixture: ComponentFixture<SignUpPage>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RegistrarPage);
+    fixture = TestBed.createComponent(SignUpPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
